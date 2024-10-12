cc950588af982a968d9207c9f6a0b241412a4c14
Pranchal Shah <pranchx@gmail.com>
2024-10-12 18:12:49 -0400

    updating headers

--- shell56_commands.h
+++ shell56_commands.h
@@ -1,5 +1,19 @@
 #include <sys/types.h>
 
+typedef struct Command {
+  char *command;
+  char **args;
+  char *input;
+  char *output;
+  struct Command *next;
+  struct Command *prev;
+} Command;
+
+Command *createCommand();
+void addCommand(Command **head, Command **tail, Command *newCmd);
+Command *buildCommandList(int n_tokens, char **tokens);
+void printCommandList(Command *head);
+
 int runcd(int argc, char **argv);
 int runpwd();
 int runexit(int argc, char **argv);
@@ -14,4 +28,4 @@ void modify_tokens_array(char **tokens, int *n_tokens, int *command_count,
                          int *isPipe);
 int checkAndRunPipes(char **tokens, int *n_tokens, char *qbuf);
 
-int runCommands(int n_tokens, char **tokens, char *qbuf);
+int runCommands(Command *head);
